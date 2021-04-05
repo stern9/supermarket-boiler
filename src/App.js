b4284f0ded39290b0b47be3315b9d9e85885bbc1
stern9 <avram.stern@digitas.com>
2021-04-05 11:04:11 -0600

    fixed styles - storefront now looks good better once user is loggedin

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
 import { useState } from "react";
-import Container from "./UI_Kit/Container.js";
-import Button from "./UI_Kit/Button.js";
 import StoreFront from "./Components/StoreFront";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
-  return (
-    <>
-      <Container className="app">
-        <StoreFront>
-          if (loggedIn)
-          {
-            <Button
-              className="btn btn-outline"
-              onClick={() => setLoggedIn(false)}
-            >
-              Logout
-            </Button>
-          }
-          else{" "}
-          {
-            <>
-              <h2>Please login</h2>
-              <Button
-                className="btn btn-primary"
-                onClick={() => setLoggedIn(true)}
-              >
-                Login
-              </Button>
-            </>
-          }
-        </StoreFront>
-      </Container>
-    </>
-  );
+  if (loggedIn) {
+    return (
+      <>
+        <StoreFront />
+        <button className="btn btn-outline" onClick={() => setLoggedIn(false)}>
+          Logout
+        </button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h2>Please login</h2>
+        <button className="btn btn-primary" onClick={() => setLoggedIn(true)}>
+          Login
+        </button>
+      </>
+    );
+  }
 }
 
 export default App;
