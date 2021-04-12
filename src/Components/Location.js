e0d10b8009c8d1ef1bb9bf97abef00ff5df1f67b
stern9 <avram.stern@digitas.com>
2021-04-12 13:14:29 -0600

    adding markers to map and different locations

--- src/Components/Location.js
+++ src/Components/Location.js
@@ -2,7 +2,7 @@ import { useState, useLayoutEffect } from "react";
 import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
 
 const Location = () => {
-  const [random, setRandom] = useState(Math.random());
+  const [marker, setMarker] = useState();
 
   mapboxgl.accessToken =
     "pk.eyJ1Ijoic3Rlcm45IiwiYSI6ImNrbmUzanE3dTA1enYycnBlcWoxNmNjNHQifQ.HUEnMQDHZV0juMkSFbBPhw";
@@ -11,17 +11,37 @@ const Location = () => {
     const map = new mapboxgl.Map({
       container: "map", // matches <div id="map" />
       style: "mapbox://styles/mapbox/dark-v10", // sets the dark mode
-      center: [-84.087502, 9.934739], // Sets the center of the map (long, lat)
+      center: [-84.11129038, 9.93691428], // Sets the center of the map (long, lat)
       zoom: 9,
     });
+    const marker = new mapboxgl.Marker()
+      .setLngLat([-84.11129038, 9.93691428])
+      .addTo(map);
+
+    setMarker(marker);
   }, []);
+
+  const stores = {
+    sanJose: [-84.11129038, 9.93691428],
+    escazu: [-84.12775103, 9.93289738],
+    heredia: [-84.12844045, 9.99920793],
+  };
+
+  const handleStoreChange = (e) => {
+    const location = e.target.value;
+    marker.setLngLat(stores[location]);
+  };
 
   return (
     <>
-      <h1>Our Store Location</h1>
-      <button id="re-render" onClick={() => setRandom(Math.random())}>
-        Re-render
-      </button>
+      <div className="map-overlay">
+        <h3>Choose store: </h3>
+        <select onChange={handleStoreChange}>
+          <option value="sanJose">San Jose</option>
+          <option value="escazu">Escazu</option>
+          <option value="heredia">Heredia</option>
+        </select>
+      </div>
       <div id="map"></div>
     </>
   );
