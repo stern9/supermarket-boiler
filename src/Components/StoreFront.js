77861255526cada194b072a940ee615bad58ce2f
stern9 <avram.stern@digitas.com>
2021-04-16 18:06:03 -0600

    refactoring, added custom useFetch hook

--- src/Components/StoreFront.js
+++ src/Components/StoreFront.js
@@ -1,30 +1,26 @@
 import React, { useEffect, useState } from "react";
 import Product from "./Product";
 import Loader from "./Loader";
+import useFetch from "./useFetch.js";
 
 export default function StoreFront() {
   const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const { get, loading } = useFetch(
+    "https://react-tutorial-demo.firebaseio.com/"
+  );
 
   useEffect(() => {
-    (async () => {
-      try {
-        const response = await fetch(
-          "https://react-tutorial-demo.firebaseio.com/products.json"
-        );
-        const data = await response.json();
-        setIsLoading(false);
+    get("products.json")
+      .then((data) => {
         setProducts(data);
-      } catch (error) {
-        console.log(error);
-        setIsLoading(false);
-      }
-    })();
+      })
+      .catch((error) => console.log(error));
+    // eslint-disable-next-line
   }, []);
 
   return (
     <div className="store-front">
-      {isLoading && <Loader />}
+      {loading && <Loader />}
       {products.map((product) => (
         <Product key={product.id} details={product} />
       ))}
